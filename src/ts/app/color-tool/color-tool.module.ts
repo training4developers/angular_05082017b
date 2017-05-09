841a554d94f91ee5e92e5c1816371ce09d9c5c93
Eric W. Greene <eric@ericwgreene.com>
2017-05-09 13:33:30 -0400

    added code demos

--- src/ts/app/color-tool/color-tool.module.ts
+++ src/ts/app/color-tool/color-tool.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
+import { CapitalizePipe } from "./pipes/capitalize.pipe";
 import { ColorToolComponent } from "./components/color-tool.component";
 
 @NgModule({
     imports: [ CommonModule, FormsModule ],
-    declarations: [ ColorToolComponent ],
+    declarations: [ ColorToolComponent, CapitalizePipe ],
     exports: [ ColorToolComponent ],
 })
 export class ColorToolModule { }
---
+++ src/ts/app/color-tool/pipes/capitalize.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: "capitalize",
+})
+export class CapitalizePipe implements PipeTransform {
+
+    public transform(value: any) {
+
+        const theValue = String(value);
+
+        return theValue.charAt(0).toUpperCase() + theValue.slice(1);
+
+    }
+}
