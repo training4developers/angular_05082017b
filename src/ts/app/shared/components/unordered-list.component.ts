f682074090722856659b3e359a7a921b8215cf8f
Eric W. Greene <eric@ericwgreene.com>
2017-05-11 15:13:32 -0400

    added code demos

--- src/ts/app/shared/components/unordered-list.component.ts
+++ src/ts/app/shared/components/unordered-list.component.ts
@@ -3,12 +3,19 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: "unordered-list",
     template: `<ul>
-        <li *ngFor="let listItem of listItems">{{listItem}}</li>
+        <li *ngFor="let listItem of listItems">
+            {{listItem}}
+            <button type="button" (click)="clickMe(listItem)">Click Me!</button>
+        </li>
     </ul>`,
 })
 export class UnorderedListComponent {
 
     @Input()
     public listItems: string[];
+
+    public clickMe(item: string) {
+        console.log(item);
+    }
 
 }
